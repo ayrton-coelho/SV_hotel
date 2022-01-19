9fa651341a8c2ca154abd8d9083e7b516d6333dd
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-01-19 01:18:46 -0300

    modulo hora pickup

---
+++ modules/pickup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""13:23:00"""
+
+def hora_pickup(hora, fecha):
+    hora_vuelo = hora.split(":")
+    h = int(hora_vuelo[0])
+    m = int(hora_vuelo[1])
+    h_flag = 0
+    D_flag = 0
+
+    # check for minute edge case
+    if m >= 30:
+        m = m - 30
+    else:
+        m = m + 30
+        h_flag = 1
+
+    # check for hour edge case
+    if h < 3:
+        if h_flag == 1:
+            h = h + 24 - 4
+        else:
+            h = h + 24 - 3
+        D_flag = 1
+    elif h == 3:
+        if h_flag == 1:
+            h = 23
+            D_flag = 1
+        else:
+            h = 0
+
+    # hora transformada exitosamente, pasar a string
+    h_str = ""
+    m_str = ""
+    if h <= 9:
+        h_str = "0" + str(h)
+    else:
+        h_str = str(h)
+    if m <= 9:
+        m_str = "0" + str(m)
+    else:
+        m_str = str(m)
+
+    # hora del pickup lista
+    pickup = h_str + ":" + m_str
+
+    # checkear fecha edge case
+    if D_flag == 1:
+        fecha = fecha.split("/")
+        dia = int(fecha[0])
+        mes = int(fecha[1])
+        año = int(fecha[2])
+        M_flag = 0
+        if dia == 1:
+            mes = mes - 1
+            M_flag = 1
+            if M_flag == 1:
+                if mes == 0:
+                    mes = 12
+                    año = año - 1
+            if (mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12):
+                dia = 31
+            elif mes == 2:
+                dia = 28
+            else:
+                dia = 30
+        else:
+            dia = dia - 1
+        pickup = pickup + " (" + str(dia) + "/" + str(mes) + "/" + str(año) + ")"
+
+    return (pickup)
